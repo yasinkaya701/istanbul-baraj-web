2654e88f28626cbe0e0b91707717da6f24f166c3
yasin <gs7016903@gmail.com>
2026-03-20 02:07:53 +0300

    Add simulation data assets for baraj_web

---
+++ assets/data/reservoir_reference.js
@@ -0,0 +1,7 @@
+window.RESERVOIR_REFERENCE = {
+  total_catchment_km2: 2688.0,
+  total_active_capacity_mcm: 868.683,
+  source: "output/reference_tables/istanbul_baraj_reference_table.csv",
+  source_page: "https://iski.istanbul/kurumsal/hakkimizda/su-kaynaklari",
+  updated_at: "2026-03-20"
+};
